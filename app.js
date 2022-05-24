bff7ecb952a49e928a0b5e41abf8994c8c952d89
John Browder <johnbrowder@Johns-MacBook-Air.local>
2022-05-24 11:32:39 -0400

    table completed

--- app.js
+++ app.js
@@ -21,3 +21,25 @@ function buildTable(data) {
     );
 });
 }
+
+// Create a function to filter the data table
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //Check to see if a date was enetered and filter the data using that date
+    if (date){
+        //Apply 'filter' to the table and keep only rows where 'datetime' matches filter value
+        filteredData = filteredData.filter(row => row.datetime == date);
+    };
+
+    // Rebuild the table using filtered data
+    buildTable(filteredData);
+};
+
+// Attache an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
